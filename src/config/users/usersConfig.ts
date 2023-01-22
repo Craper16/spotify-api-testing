64beec20697565464104dd6f50356c1cc6c0ebfa
Georgio <georgio.saad@gmail.com>
2023-01-22 02:00:13 +0200

    seperating searches

--- src/config/users/usersConfig.ts
+++ src/config/users/usersConfig.ts
@@ -1,10 +1,6 @@
 import { instance } from '../config';
 
-interface createPlaylistData {
-  name: string;
-  description: string;
-  public: boolean;
-}
+
 
 export const getUser = async () => {
   const access_token = localStorage.getItem('access_token');
@@ -16,45 +12,4 @@ export const getUser = async () => {
       return response;
     })
     .catch((error) => error.error?.message || 'An Error has occured');
-};
-
-export const getUserPlaylists = async () => {
-  const access_token = localStorage.getItem('access_token');
-  const userId = localStorage.getItem('userId');
-  if (!userId) {
-    return console.log('Could not fetch userId');
-  }
-
-  return await instance
-    .get(`/users/${userId}/playlists`, {
-      headers: { Authorization: `Bearer ${access_token}` },
-    })
-    .then((response) => {
-      console.log(response);
-      return response;
-    })
-    .catch((error) => error?.error?.message || 'An Error has occured');
-};
-
-export const createUserPlayList = async (data: createPlaylistData) => {
-  const access_token = localStorage.getItem('access_token');
-  const userId = localStorage.getItem('userId');
-
-  if (!access_token) {
-    return console.log('No Access Token');
-  }
-
-  if (!userId) {
-    return console.log('Could not fetch userId');
-  }
-
-  return await instance
-    .post(`/users/${userId}/playlists`, data, {
-      headers: { Authorization: `Bearer ${access_token}` },
-    })
-    .then((response) => {
-      console.log(response);
-      return response;
-    })
-    .catch((error) => error?.error?.message || 'An Error has occured');
 };
