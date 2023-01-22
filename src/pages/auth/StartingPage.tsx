a03364fb742f1f1415090d85e0d40bc7730c0bcc
Georgio <georgio.saad@gmail.com>
2023-01-22 19:05:52 +0200

    Fetching user data on login

--- src/pages/auth/StartingPage.tsx
+++
@@ -1,9 +0,0 @@
-import React, { useEffect } from 'react';
-
-import { useAppDispatch } from '../../redux/hooks';
-
-
-export default function StartingPage() {
-  const dispatch = useAppDispatch();
-
-}
