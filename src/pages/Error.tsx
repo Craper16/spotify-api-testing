a96de57e9ca6f7f8f88eb30b5cb8d07a139d46c5
Georgio <georgio.saad@gmail.com>
2023-01-23 01:42:51 +0200

    adding routes

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export default function Error() {
-  return <div>Error</div>;
+  return (
+    <div style={{ color: 'tomato', textAlign: 'center', fontWeight: 'bold' }}>
+      Error 404 page not found
+    </div>
+  );
 }
